4bfdc5fcc9c1da1d0ed2c0225394c4ea512da0fa
Pradeep Adhikari <adhikaripradeep11@gmail.com>
2023-03-16 12:43:45 +0545

    earbuds api update

---
+++ Homedata/allEarbuds.js
@@ -0,0 +1,51 @@
+
+
+const allEarbuds = [
+    {
+      id: 1,
+      Img: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678948613/Yantram%20Nepal/All%20Sounds/t1/t1_default_pjmpf3.jpg",
+      hoverImg: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678948613/Yantram%20Nepal/All%20Sounds/t1/t1hover_v3slpu.jpg",
+      buttonhoverImg: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678948613/Yantram%20Nepal/All%20Sounds/t1/t1_bhover_i11ull.jpg",
+      title: "acefast t1",
+      productName: "earbud",
+      price: "NRS 749",
+    },
+    {
+      id: 2,
+      Img: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678948998/Yantram%20Nepal/All%20Sounds/h1/h1default_vtbecb.jpg",
+      hoverImg: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678948999/Yantram%20Nepal/All%20Sounds/h1/h1hover_a8ekfd.jpg",
+      buttonhoverImg: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678948998/Yantram%20Nepal/All%20Sounds/h1/h1bhover_cb1nxz.jpg",
+      title: "acefast h1",
+      productName: "Headphone",
+      price: "NRS 749",
+    },
+    {
+      id: 3,
+      Img: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678949199/Yantram%20Nepal/All%20Sounds/t2/t2_default_fdf7pl.jpg",
+      hoverImg: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678949199/Yantram%20Nepal/All%20Sounds/t2/t2hover_tgsfyq.jpg",
+      buttonhoverImg: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678949199/Yantram%20Nepal/All%20Sounds/t2/t2_default_fdf7pl.jpg",
+      title: "acefast t2",
+      productName: "earbud",
+      price: "NRS 749",
+    },
+    // {
+    //   id: 4,
+    //   Img: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678790496/Yantram%20Nepal/allEarbuds/bluetooth-headphones_d4iugr.jpg",
+    //   hoverImg: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678790496/Yantram%20Nepal/allEarbuds/bluetooth-headphones_d4iugr.jpg",
+    //   buttonhoverImg:"https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678790496/Yantram%20Nepal/allEarbuds/bluetooth-headphones_d4iugr.jpg",
+    //   title: "acefast t8",
+    //   productName: "earbud",
+    //   price: "NRS 749",
+    // },
+    // {
+    //   id: 5,
+    //   Img: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678790496/Yantram%20Nepal/allEarbuds/bluetooth-headphones_d4iugr.jpg",
+    //   hoverImg: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678790496/Yantram%20Nepal/allEarbuds/bluetooth-headphones_d4iugr.jpg",
+    //   buttonhoverImg: "https://res.cloudinary.com/tuddrom-nepal/image/upload/v1678790496/Yantram%20Nepal/allEarbuds/bluetooth-headphones_d4iugr.jpg",
+    //   title: "acefast t2",
+    //   productName: "earbud",
+    //   price: "NRS 749",
+    // },
+  ];
+  
+  export default allEarbuds
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import router from './router/route.js'
 import adminRoutes from './router/admin/auth.js';
 import categoryRoutes from "./router/category.js";
 import allSounds from "./Homedata/allSounds.js"
+import allEarbuds from "./Homedata/allEarbuds.js"
 import cors from "cors";
 
 
@@ -30,9 +31,9 @@ mongoose.connect(connection_url, {
 
 //api endpoints
 app.get("/", (req, res) => res.status(200).send("hello world"));
-
 app.get('/v1/posts', (req, res) => res.status(200).send(Data))
 app.get('/v1/allsounds', (req, res) => res.status(200).send(allSounds))
+app.get('/v1/allsounds', (req, res) => res.status(200).send(allEarbuds))
 
 app.post('/v2/posts', (req, res) => {
     const dbVideos = req.body
