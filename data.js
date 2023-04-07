733316f3d10d356a27f64a9bb1d0a7772f2f03a9
PradeepGorkhali <55340536+PradeepGorkhali@users.noreply.github.com>
2023-04-07 12:12:59 +0545

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const Data = [
   {
     id: 1,
     Img: "https://rodhisources.com/productsources.png",
-    firsttitle: "Product Sourcing",
+    firsttitle: "Product Source",
     paragraph: "Product sourcing is the process of purchasing the best products to sell. The products are often bought from reliable retail suppliers and manufacturers at a reasonable price. The method includes researching,calculating the product’s price and acquisition costs, vettingreputable suppliers, and negotiating to get the best deals.Ultimately, your product sourcing strategy should lead you to findquality products at prices that allow for significant profit margins.So here we are with over a decade of experience in the sourcing arena,our knowledge and skills are sound. We believe we can help you findthe right supplier even for the rarest of goods. Once you submit you inquiry, our specialist research team in China, which has beensourcing for over ten years, will find the best products from a propermanufacturer for you through our supply channel and notify you within one business day.",
     secondtitle: "What do you get with our Sourcing service?",
     li1: "Product and market ideas and strategies",
